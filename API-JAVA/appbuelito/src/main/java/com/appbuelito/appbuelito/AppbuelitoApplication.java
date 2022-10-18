5593817c82c6dea6d75f7fc2a50ed5fda0af483b
Bruno Cosimano <82295361+BrunoLCA95@users.noreply.github.com>
2022-10-17 21:59:21 -0300

    Plantilla api java

---
+++ API-JAVA/appbuelito/src/main/java/com/appbuelito/appbuelito/AppbuelitoApplication.java
@@ -0,0 +1,13 @@
+package com.appbuelito.appbuelito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppbuelitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppbuelitoApplication.class, args);
+	}
+
+}
---
+++ API-JAVA/appbuelito/src/test/java/com/appbuelito/appbuelito/AppbuelitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appbuelito.appbuelito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppbuelitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
