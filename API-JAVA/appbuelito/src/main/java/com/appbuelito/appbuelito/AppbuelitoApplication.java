60e09213c8b7469c2e1db123ab1f9fd62005bd6e
Noelia Romina Ruiz <estefitrinidad@gmail.com>
2022-10-17 23:17:45 -0300

    Añadiendo clase Appbelito y creación de login y registro.

--- API-JAVA/appbuelito/src/main/java/com/appbuelito/appbuelito/AppbuelitoApplication.java
+++ API-JAVA/appbuelito/src/main/java/com/appbuelito/appbuelito/AppbuelitoApplication.java
@@ -8,6 +8,9 @@ public class AppbuelitoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppbuelitoApplication.class, args);
+		Appbuelito persona1 = new Appbuelito("User", "default", "email@example.com", "12345");
+		persona1.mostrarLogin();
+		persona1.registro();
 	}
 
 }
